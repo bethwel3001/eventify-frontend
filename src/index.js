bc843ec94e2790bcd14b518d3ebd47d75ec6bb11
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-09-19 13:55:40 +0300

    added browserouter for nav

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom'; // Ensure this is imported
+import { BrowserRouter as Router } from 'react-router-dom'; 
 
 ReactDOM.render(
   <Router>
